7078596255d49d2b3e2274404b70ace39e87db87
chri70y9 <chri70y9@VIDF1Y6G72>
2016-12-29 19:38:02 +0100

    repo added

---
+++ DmRepo/Conn.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.SqlClient;
+
+namespace DmRepo
+{
+    public static class Conn
+    {
+        /// <summary>
+        /// Metoden indeholder formindelsesindstillinger til databaen
+        /// </summary>
+        /// <returns>Retunere en MS SQL connection string</returns>
+        public static SqlConnection GetCon()
+        {
+            SqlConnection con = new SqlConnection("server=mssql7.unoeuro.com;database=amhs_dk_db;uid=amhs_dk;pwd=hxmkna62;MultipleActiveResultSets=True");
+            return con;
+        }
+
+        /// <summary>
+        /// Metoden retunere en åben forbindelse til databasen der er defineret i GetCon()
+        /// </summary>
+        /// <returns>Retunere en åben forbindelse til databasen</returns>
+        public static SqlConnection CreateConnection()
+        {
+            var cn = GetCon();
+            cn.Open();
+            return cn;
+        }
+    }
+}
