1764e0d03d8af06e3f49d2f36d381bd0c94fbe35
Danni Rosing <rosingdanni@gmail.com>
2017-01-05 12:44:29 +0100

    sasas

--- DmRepo/Models/Gmap.cs
+++ DmRepo/Models/Gmap.cs
@@ -8,5 +8,13 @@ namespace DmRepo
 {
      public class Gmap
     {
+         public int ID { get; set; }
+         public string Googlelat { get; set; }
+         public string Googlelng { get; set; }
+         public string Wgs84lat { get; set; }
+         public string Wgs84lng { get; set; }
+         public string Beskrivelse { get; set; }
+         public string Type { get; set; }
+         public string Overskrift { get; set; }
     }
 }
--- DykkerMania/Controllers/HomeController.cs
+++ DykkerMania/Controllers/HomeController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DmRepo;
 
 namespace DykkerMania.Controllers
 {
     public class HomeController : Controller
     {
+        GmapFac gf = new GmapFac();
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View(gf.GetAll());
         }
 
 
